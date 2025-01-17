c6125343078d16f57e4897f7eae364be0e960f7b
dolev goaz <www.dolev@gmail.com>
2025-01-17 21:48:20 +0200

    nested array comparison

--- src/test_utils/array.ts
+++ src/test_utils/array.ts
@@ -2,7 +2,9 @@
 export function compareArrays<T>(a1: T[], a2: T[]) {
     if (a1.length != a2.length) return false;
     for(let i = 0; i < a1.length; ++i) {
-        if (a1[i] != a2[i]) return false;
+        if (Array.isArray(a1[i]) && Array.isArray(a2[i])) {
+            if (!compareArrays(a1[i] as any[], a2[i]  as any[])) return false;
+        } else if (a1[i] != a2[i]) return false;
     }
     return true;
 }
