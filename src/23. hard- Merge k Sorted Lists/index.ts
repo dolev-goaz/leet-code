130440ebf0dd32737a694634410ad4ae80b0b5a0
dolev goaz <www.dolev@gmail.com>
2025-01-17 19:22:11 +0200

    divide & conquer

--- src/23. hard- Merge k Sorted Lists/index.ts
+++ src/23. hard- Merge k Sorted Lists/index.ts
@@ -10,32 +10,69 @@ class ListNode {
     }
 }
 
-function findMin(lists: Array<ListNode | null>) {
-    let min = 1e5; // min value
-    let minIndex = -1;
-    for(let i = 0; i < lists.length; ++i) {
-        const list = lists[i];
-        if (!list) continue;
-        if (list.val < min) {
-            min = list.val;
-            minIndex = i;
+// function findMin(lists: Array<ListNode | null>) {
+//     let min = 1e5; // min value
+//     let minIndex = -1;
+//     for(let i = 0; i < lists.length; ++i) {
+//         const list = lists[i];
+//         if (!list) continue;
+//         if (list.val < min) {
+//             min = list.val;
+//             minIndex = i;
+//         }
+//     }
+//     return minIndex;
+// }
+
+// function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+//     if (lists.every((list) => !list)) return null; // if all lists are null
+//     const dummy = new ListNode();
+//     let current = dummy;
+//     while (true){
+//         const minIndex = findMin(lists);
+//         if (minIndex == -1) return dummy.next;
+//         current.next = lists[minIndex];
+//         current = current.next!;
+//         lists[minIndex] = lists[minIndex]!.next;
+//     }
+// };
+
+function mergeSort2(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    if (!l1 || !l2) return l1 ?? l2;
+    
+    const dummy = new ListNode();
+    let head = dummy;
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            head.next = l1;
+            head = head.next;
+            l1 = l1.next;
+            head.next = null;
+        } else {
+            head.next = l2;
+            head = head.next;
+            l2 = l2.next;
+            head.next = null;
         }
     }
-    return minIndex;
+
+    head.next = (l1 ?? l2);
+    return dummy.next;
+}
+
+function mergeSortDivideAndConquer(lists: Array<ListNode | null>, left: number, right: number): ListNode | null {
+    if (left == right) return lists[left];
+    const mid = Math.floor((left + right) / 2);
+    const l1 = mergeSortDivideAndConquer(lists, left, mid);
+    const l2 = mergeSortDivideAndConquer(lists, mid+1, right);
+    return mergeSort2(l1, l2);
 }
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-    if (lists.every((list) => !list)) return null; // if all lists are null
-    const dummy = new ListNode();
-    let current = dummy;
-    while (true){
-        const minIndex = findMin(lists);
-        if (minIndex == -1) return dummy.next;
-        current.next = lists[minIndex];
-        current = current.next!;
-        lists[minIndex] = lists[minIndex]!.next;
-    }
+    if (lists.length == 0) return null;
+    return mergeSortDivideAndConquer(lists, 0, lists.length - 1);
 };
+
 
 function methodWrapper(lists: Array<number[]>) {
     const nodeLists = lists.map(createListNode);
